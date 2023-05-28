e1763beaa4dd296eb416ad43a4e361c2e779ace3
Santiago Gil Ramirez <90258568+Santigil0814@users.noreply.github.com>
2023-05-28 12:52:51 -0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const btnMenu = document.querySelector(".btmMenu");
+const menuNav = document.querySelector(".menu");
+
+// Abrir cerrar nav
+btnMenu.addEventListener("click", () => {
+  menuNav.classList.toggle("active");
+});
+
+// Cerrar al dar click en link
+document.querySelectorAll("nav-links").forEach(
+  (link) => link.addEventListener("click"),
+  () => {
+    menuNav.classList.remove("active");
+  }
+);
+
+// Botón seleccionado
+var botones = menuNav.getElementsByClassName("nav-links");
+for (var i = 0; i < botones.length; i++) {
+  botones[i].addEventListener("click", function () {
+    var select = document.getElementsByClassName("active");
+    select[0].className = select[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// Ir a incio
+const subir = document.querySelector(".subir");
+subir.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
